41a0cbc2aaf937d5b627498ef84ee67f6803fe69
Seyed Razavi <monkeyx@gmail.com>
2015-10-26 21:56:31 +0000

    Reduce default retry attempts

--- PhoenixPCL/SAL/NexusRequest.cs
+++ PhoenixPCL/SAL/NexusRequest.cs
@@ -23,6 +23,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -256,7 +257,12 @@ namespace Phoenix.SAL
             return BASE_URL + "&uid=" + UID + "&code=" + Code + "&sa=" + Action + "&tid=" + PositionId + "&pid=" + PositionId;
         }
 
-		private const int maxAttempts = 3;
+		#if NEXUS_ATTEMPT_COUNT
+		private const int maxAttempts = NEXUS_ATTEMPT_COUNT;
+		#else
+		private const int maxAttempts = 1;
+		#endif
+
 		private int attemptCount = 0;
     }
 }
